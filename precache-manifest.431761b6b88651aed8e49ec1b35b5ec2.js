9cf562b9f78dbb9e9c7a742fa98eeea6c27cc374
AbdelTrochez <abdel.atr@gmail.com>
2020-06-05 23:43:01 -0600

    Updates

---
+++ precache-manifest.431761b6b88651aed8e49ec1b35b5ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5dcce0efbb8b2841c117dc531e8d8a",
+    "url": "/chucknorrisjokes/index.html"
+  },
+  {
+    "revision": "63404b80c337caae9ef7",
+    "url": "/chucknorrisjokes/static/js/2.34892ae8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chucknorrisjokes/static/js/2.34892ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345a72dae4aa67e29591",
+    "url": "/chucknorrisjokes/static/js/main.b91a6e65.chunk.js"
+  },
+  {
+    "revision": "016b93607932c5a89ecd",
+    "url": "/chucknorrisjokes/static/js/runtime-main.2d5257af.js"
+  },
+  {
+    "revision": "872baa13ee8bfb7c716df1e6f38fa86d",
+    "url": "/chucknorrisjokes/static/media/chuck.872baa13.png"
+  }
+]);
